821dd1b74ec996ff9bd1795afc9284a0c637c88e
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2024-06-01 20:32:59 +0700

    feat: update api, fix: typo poll -> product (#5)

--- internal/data/gallery_db/model.go
+++ internal/data/gallery_db/model.go
@@ -1,7 +1,7 @@
 package gallery_db
 
 type Product struct {
-	ID          int64  `json:"id,omitempty"`
+	Id          int64  `json:"id,omitempty"`
 	Title       string `json:"title,omitempty"`
 	ImageUrl    string `json:"image_url,omitempty"`
 	Description string `json:"description,omitempty"`
