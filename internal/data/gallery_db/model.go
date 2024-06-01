f67c928fbc5b7e8b184c4611a3ff78d147ec2bd8
mlutfifahreza <mlutfifahreza@gmail.com>
2024-06-01 19:26:32 +0700

    fix: product json tag

--- internal/data/gallery_db/model.go
+++ internal/data/gallery_db/model.go
@@ -1,8 +1,8 @@
 package gallery_db
 
 type Product struct {
-	ID          int64
-	Title       string
-	ImageURL    string
-	Description string
+	ID          int64  `json:"id,omitempty"`
+	Title       string `json:"title,omitempty"`
+	ImageURL    string `json:"image_url,omitempty"`
+	Description string `json:"description,omitempty"`
 }
